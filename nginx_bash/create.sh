a62682de62f463636e634ee09bc3f22cbdf6c4ca
AaronEnglish <aaenglis@gmail.com>
2018-11-29 05:51:18 -0600

    Update create.sh
    rolled back to pointing to older nginx version, commented out and changed source from which nginx is grabbed

--- nginx_bash/create.sh
+++ nginx_bash/create.sh
@@ -1,14 +1,43 @@
-sudo apt-get -y install build-essential libpcre3 libpcre3-dev libssl-dev libaio1 libaio-dev jq
+sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev libaio1 libaio-dev
+
 cd /home
-#wget http://nginx.org/download/nginx-1.15.6.tar.gz && tar -zxvf nginx-1.15.6.tar.gz
+
+#sudo rm -r ./nginx
+
+sudo rm -r ./nginx-rtmp-module
+
+wget http://nginx.org/download/nginx-1.15.6.tar.gz && tar -zxvf nginx-1.15.6.tar.gz
+
 nginx_release=$(curl -s 'https://api.github.com/repos/nginx/nginx/tags' | jq -r '.[0] | .name')
+
+nginx_release=${nginx_release##*-}
+
+sudo rm -r ./"nginx-$nginx_release"
+
+sudo rm -f ./"nginx-$nginx_release".tar.gz
+
 RTMPVERSION=$(curl -s 'https://api.github.com/repos/sergey-dryabzhinsky/nginx-rtmp-module/tags' | jq -r '.[0] | .name')
-sudo git clone --branch $nginx_release https://github.com/nginx/nginx.git
+
+#git clone --branch $nginx_release https://github.com/nginx/nginx.git
+
+#wget http://nginx.org/download/"nginx-$nginx_release".tar.gz && tar -zxvf "nginx-$nginx_release".tar.gz
+
+echo "nginx-$nginx_release"
+
 #wget https://www.zlib.net/zlib-1.2.11.tar.gz && tar xzvf zlib-1.2.11.tar.gz
-sudo git clone --branch $RTMPVERSION https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
-cd ./nginx
+
+git clone --branch $RTMPVERSION https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
+
+#cd ./"nginx-$nginx_release"
+
+cd ./nginx-1.15.6
+
 ./configure --with-http_ssl_module --with-file-aio --without-http_gzip_module --add-module=../nginx-rtmp-module
+
 make
+
 sudo make install clean
+
 echo $nginx_release
+
 echo $RTMPVERSION
