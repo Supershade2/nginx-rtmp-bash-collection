2544dc0190ec0daa129f23bacd0eb025019fe4fd
AaronEnglish <aaenglis@gmail.com>
2018-11-30 04:16:23 -0600

    Update create.sh

--- nginx_bash/create.sh
+++ nginx_bash/create.sh
@@ -6,29 +6,30 @@ cd /home
 
 sudo rm -r ./nginx-rtmp-module
 
-wget http://nginx.org/download/nginx-1.15.6.tar.gz && tar -zxvf nginx-1.15.6.tar.gz
+#wget http://nginx.org/download/nginx-1.15.6.tar.gz && tar -zxvf nginx-1.15.6.tar.gz
 
-nginx_release=$(curl -s 'https://api.github.com/repos/nginx/nginx/tags' | jq -r '.[0] | .name')
+#nginx_release=$(curl -s 'https://api.github.com/repos/nginx/nginx/tags' | jq -r '.[0] | .name')
 
-nginx_release=${nginx_release##*-}
+#nginx_release=${nginx_release##*-}
+nginx_release="release-1.9.15";
 
-sudo rm -r ./"nginx-1.15.6"
+sudo rm -r ./nginx
 
-sudo rm -f ./"nginx-1.15.6".tar.gz
+#sudo rm -f ./"nginx-1.15.6".tar.gz
 
-#RTMPVERSION=$(curl -s 'https://api.github.com/repos/sergey-dryabzhinsky/nginx-rtmp-module/tags' | jq -r '.[1] | .name')
+RTMPVERSION=$(curl -s 'https://api.github.com/repos/sergey-dryabzhinsky/nginx-rtmp-module/tags' | jq -r '.[0] | .name')
 
-#git clone --branch $nginx_release https://github.com/nginx/nginx.git
+git clone --branch $nginx_release https://github.com/nginx/nginx.git
 
 #wget http://nginx.org/download/"nginx-$nginx_release".tar.gz && tar -zxvf "nginx-$nginx_release".tar.gz
 
 #wget https://www.zlib.net/zlib-1.2.11.tar.gz && tar xzvf zlib-1.2.11.tar.gz
 
-git clone --branch dev https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
+git clone --branch $RTMPVERSION https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
 
 #cd ./"nginx-$nginx_release"
 
-cd ./nginx-1.15.6
+cd ./nginx
 
 ./configure --with-http_ssl_module --with-file-aio --without-http_gzip_module --add-module=../nginx-rtmp-module
 
