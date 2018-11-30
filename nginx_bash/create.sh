ac0867d6e130b987f4108ba91c60e8fcff64b29e
AaronEnglish <aaenglis@gmail.com>
2018-11-30 02:51:12 -0600

    Update create.sh

--- nginx_bash/create.sh
+++ nginx_bash/create.sh
@@ -6,13 +6,13 @@ nginx_release=${nginx_release##*-}
 sudo rm -r ./"nginx-$nginx_release"
 sudo rm -f ./"nginx-$nginx_release".tar.gz
 sudo rm -r ./nginx-rtmp-module
-RTMPVERSION=$(curl -s 'https://api.github.com/repos/sergey-dryabzhinsky/nginx-rtmp-module/tags' | jq -r '.[0] | .name')
+#RTMPVERSION=$(curl -s 'https://api.github.com/repos/sergey-dryabzhinsky/nginx-rtmp-module/tags' | jq -r '.[0] | .name')
 wget http://nginx.org/download/"nginx-$nginx_release".tar.gz && tar -zxvf "nginx-$nginx_release".tar.gz
 #wget https://www.zlib.net/zlib-1.2.11.tar.gz && tar xzvf zlib-1.2.11.tar.gz
-sudo git clone --branch $RTMPVERSION https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
+sudo git clone --branch dev https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
 cd ./"nginx-$nginx_release"
 ./configure --with-http_ssl_module --with-file-aio --without-http_gzip_module --add-module=../nginx-rtmp-module
 make
 sudo make install clean
 echo $nginx_release
-echo $RTMPVERSION
+#echo $RTMPVERSION
