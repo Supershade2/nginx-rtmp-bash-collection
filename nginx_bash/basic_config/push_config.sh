27ee7f2a9fa2c73395ee93c5016fb1825f881720
AaronEnglish <aaenglis@gmail.com>
2018-12-07 04:17:36 -0600

    Delete push_config.sh

--- nginx_bash/basic_config/push_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir backup
-sudo cp /usr/local/nginx/conf/nginx.conf ./backup
-sudo cp /home/nginx-rtmp-bash-collection/nginx_bash/basic_config/nginx.conf /usr/local/nginx/conf/
-reload.sh
